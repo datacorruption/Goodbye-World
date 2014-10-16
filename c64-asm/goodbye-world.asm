14fedd6ee6d8884651523c606694fd58675c07c0
kikendo <k.drone.1@gmail.com>
2014-10-15 21:52:19 -0400

    Update goodbye-world.asm
    Added the proper BASIC loader with all the right ACME syntax.
    Tested 100% running on a C64!

--- c64-asm/goodbye-world.asm
+++ c64-asm/goodbye-world.asm
@@ -1,16 +1,24 @@
-
 ;====================
 ; Goodbye World!!!!! 
 ;====================
-			*= $1000			; Memory address to load the code at.	
+
+!to "goodbye-world.prg", cbm
+
+;BASIC loader
+*= $801 ; start at c64 basic start
+!byte $b, $08, $EF, $00, $9E, $32, $30, $36,$31, $00, $00, $00 ; basic header
+
 			ldx #$00;
 LOOP		lda textcontent,x ; 
 			sta $0400,x	
 			inx	
-			cpx #$0c
+			cpx #$0d
 			BNE LOOP
 			rts
-						
+					
+;end of main loop
+
 ;-------------------------------------- data portion
 		
-textcontent			!SCR "Goodbye World"
+textcontent			!SCR "goodbye world"
+
